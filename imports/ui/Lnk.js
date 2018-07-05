dcaec963ade2c89c7674ca09e5f6dbda2028bf27
Mehdi Mohammadi <m.mohammadi@hiddenboomerang.com>
2018-07-05 14:16:40 +0100

    Add meteor accounts-password moudle

--- imports/ui/Lnk.js
+++ imports/ui/Lnk.js
@@ -4,6 +4,7 @@ export default class Lnk extends Component {
   render () {
     return (
       <div>
+        <h1>Short Lnk Links</h1>
         <button onClick={() => { this.props.history.push('/') }}>
         Logout
         </button>
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Signup extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      error: ''
+    }
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+
+    this.setState({
+      error: 'Something went wrong!'
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>Join Short Lnk</h1>
-
-      Login Form Here
-
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type='email' name='email' placeholder='Email' />
+          <input type='password' name='password' placeholder='Password' />
+          <button>Create Account</button>
+        </form>
         <Link to='/'>Already have an account!</Link>
       </div>
     )
