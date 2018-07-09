df9a9bd72c719e84ce20be401852bb2d6fdbd5bc
Mehdi Mohammadi <m.mohammadi@hiddenboomerang.com>
2018-07-09 22:39:35 +0100

    Add the route lecture 59

--- imports/routes/AppRouter.js
+++ imports/routes/AppRouter.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Meteor } from 'meteor/meteor'
 import { Router, Route, Switch } from 'react-router-dom'
 import createHistory from 'history/createBrowserHistory'
+import { Tracker } from 'meteor/tracker'
 
 import Signup from '../ui/Signup'
 import NotFound from '../ui/NotFound'
 import Login from '../ui/Login'
 import Lnk from '../ui/Lnk'
 
-export const history = createHistory()
+const history = createHistory()
+
+const unathenticatedPages = ['/', '/signup']
+const authenticatedPages = ['/links']
 
 export const AppRouter = () => (
   <Router history={history}>
@@ -18,4 +23,18 @@ export const AppRouter = () => (
       <Route path='*' component={NotFound} />
     </Switch>
   </Router>
+)
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId()
+  const pathName = history.location.pathname
+  const isUnauthenticatedPage = unathenticatedPages.includes(pathName)
+  const isAuthenticatedPage = authenticatedPages.includes(pathName)
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    history.push('/links')
+  } else if (isAuthenticatedPage && !isAuthenticated) {
+    history.push('/')
+  }
+}
 )
--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -9,31 +9,25 @@ class Login extends Component {
       error: ''
     }
   }
-
   onSubmit (e) {
     e.preventDefault()
-
     let email = this.refs.email.value.trim()
     let password = this.refs.password.value.trim()
     Meteor.loginWithPassword({email}, password, (err) => {
       console.log('signup callback', err)
     })
-
-    // this.setState({
-    //   error: 'Something went wrong!'
-    // })
   }
-
   render () {
     return (
       <div>
-        <h1>Login to Short Lnk</h1>
-        {/* {this.state.error ? <p>{this.state.error}</p> : undefined} */}
+        <h1>Short Lnk</h1>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
         <form onSubmit={this.onSubmit.bind(this)}>
           <input type='email' ref='email' name='email' placeholder='Email' />
           <input type='password' ref='password' name='password' placeholder='Password' />
           <button>Login</button>
-        </form>        <Link to='/signup'>Don't have an account?</Link>
+        </form>
+        <Link to='/signup'>Don't have an account?</Link>
       </div>
     )
   }
